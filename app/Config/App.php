ecbaf818763d0ed132904001dc0578af7057237a
ddhyz <dheadhea978@gmail.com>
2026-06-26 18:32:40 +0700

    Remove index.php from URLs

--- app/Config/App.php
+++ app/Config/App.php
@@ -51,7 +51,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $indexPage = 'index.php';
+    public $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
