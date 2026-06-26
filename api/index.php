f081e0e9874fab8737d1e71bcc61735864255498
ddhyz <dheadhea978@gmail.com>
2026-06-26 17:20:15 +0700

    Force development mode to debug

--- api/index.php
+++ api/index.php
@@ -1,3 +1,6 @@
 <?php
+// Force development mode for debugging
+$_SERVER['CI_ENVIRONMENT'] = 'development';
+
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
